453fea1a753319d568ac95e6ed2e68b5e88e800d
= <=>
2024-07-31 18:45:49 -0300

    Add project files.

---
+++ CleanArch.Infra.Data/EntityConfigurations/ProductConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArch.Infra.Data.EntityConfigurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Caderno",
+                    Description = "Caderno Espiral 100 folhas",
+                    Price = 9.50m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Borracha",
+                    Description = "Borracha Branca pequena",
+                    Price = 3.75m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "",
+                    Description = "Caderno Universitário 500 folhas",
+                    Price = 59.50m
+                }
+                );
+        }
+    }
+}
---
+++ CleanArch.Infra.Ioc/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Infra.Data.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.Ioc
+
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext)
+                    .Assembly.FullName)));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
