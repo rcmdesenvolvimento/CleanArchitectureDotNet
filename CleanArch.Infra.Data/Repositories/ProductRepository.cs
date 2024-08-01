d4950b25b8aeaa677f373a661b7c4af1f547de8f
= <=>
2024-07-31 23:46:44 -0300

    Criação da tabela products

---
+++ CleanArch.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using System.Collections.Generic;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products;
+        }
+    }
+}
